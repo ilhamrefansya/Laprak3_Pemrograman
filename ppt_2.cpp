a4e5e7cd620c80091b19ef86aff1f481848941bf
ilhamrefansya <ilhamrefansya@gmail.com>
2025-09-23 17:25:37 +0700

    Update ppt_2.cpp

--- ppt_2.cpp
+++ ppt_2.cpp
@@ -1,1 +1,27 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+  int a, b, op;
+cout << "masukkan bilangan 1: " << endl;
+  cin >> a;
+cout << "masukkan bilangan 2: " << endl;
+  cin >> b;
+cout << "masukkan operasi 1(+), 2(-), 3(*), 4(/): " << endl;
+cin >> op;
+  switch  (op) {
+    case 1:
+    cout << "hasil: " << a + b << endl;
+    break;
+    case 2:
+    cout << "hasil: " << a - b << endl;
+    break;
+    case 3:
+    cout << "hasil: " << a * b << endl;
+    break;
+    case 4:
+    cout << "hasil: " << a / b << endl;
+    break;
+    default:
+    cout << "operasi tidak valid" << endl;  
+}
+}
